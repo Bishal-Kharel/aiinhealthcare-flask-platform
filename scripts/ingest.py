e96c149b38c809bcd931c14990b659126f9621b4
Bishal Kharel <bishalkharel9842@gmail.com>
2025-05-29 14:35:40 +0545

    Updated import for smooth functionality

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import TextLoader, PyPDFLoader, CSVLoader, UnstructuredWordDocumentLoader, UnstructuredMarkdownLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 import os
 
 
